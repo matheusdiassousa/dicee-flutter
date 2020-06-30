348fbf41f0c59fb0712312abe3bf1a00b1b2adea
Matheus <matheus_diassousa@hotmail.com>
2020-06-29 22:46:32 -0300

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
 
 void main() {
   return runApp(
@@ -15,9 +17,51 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int numbl = 1;
+  int numbr = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                diceChange();
+                print('Left Button was pressed. Random Number $numbl');
+              },
+              child: Image.asset('images/dice$numbl.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                diceChange();
+                print('Right Button was pressed. Random Number $numbr');
+              },
+              child: Image.asset('images/dice$numbr.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+  diceChange(){
+  return setState(() {
+      numbr = 1 + Random().nextInt(6);
+      numbl = 1 + Random().nextInt(6);
+      });
   }
 }
+
+
+
